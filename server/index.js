656cad7d2c44ad4c386083a1c5b2059bad4fc02f
shabbir <kaderishabbir@gmail.com>
2024-01-03 20:08:08 +0530

    finally

--- server/index.js
+++ server/index.js
@@ -8,24 +8,23 @@ const WEATHER_API_ENDPOINT = "https://open-weather13.p.rapidapi.com";
 app.use(bodyParser.json());
 // app.use(cors());
 
-function convert(f) {
-  return (((f - 32) * 5) / 9).toFixed(2);
-}
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.post("/getWeather", async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
   // another common pattern
   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
   try {
     const { cities } = req.body;
     // console.log("HEYYY");
@@ -39,17 +38,17 @@ app.post("/getWeather", async (req, res) => {
       const formattedCity = city.trim().toLowerCase().replace(" ", "+");
       const apiRes = await axios.request({
         method: "GET",
-        url: 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather',
-        params: {city: formattedCity},
+        url: "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather",
+        params: { city: formattedCity },
         headers: {
-          'X-RapidAPI-Key': '2d067bedbbmsh9dfcb9607842411p164b03jsn566de3609656',
-          'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-        }
+          "X-RapidAPI-Key":
+            "2d067bedbbmsh9dfcb9607842411p164b03jsn566de3609656",
+          "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+        },
       });
-        console.log(apiRes, formattedCity);
-      const apiResData = await apiRes.json();
-      const temp = convert(apiResData.temp);
-      console.log("Temperature in Celsius:", temp, formattedCity);
+      // console.log(apiRes, formattedCity);
+      const temp = apiRes.data.temp;
+      // console.log("Temperature in Celsius:", temp, formattedCity);
       weatherData[city] = `${temp} °C`;
     }
     // res.json({ weather: weatherData });
